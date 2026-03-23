3d1611dd12a453d978e5b63488a299b8abd0bca7
Anubhav Singh <thakuranubhav623@gmail.com>
2026-03-23 12:07:07 +0530

    refactor: auth screen updated

--- app/src/main/java/com/anucodes/connecto/presentation/shared/CircularLoadingBar.kt
+++ app/src/main/java/com/anucodes/connecto/presentation/shared/CircularLoadingBar.kt
@@ -1,24 +1,23 @@
 package com.anucodes.connecto.presentation.shared
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun CircularLoadingBar (
+fun CircularLoadingBar(
     isVisible: Boolean,
     scrimColor: Color = Color.Black.copy(alpha = 0.6f),
     indicatorColor: Color? = null,
@@ -27,17 +26,19 @@ fun CircularLoadingBar (
 ) {
     if (!isVisible) return
 
-    val blockInteractionSource = remember { MutableInteractionSource() }
-
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(scrimColor)
-            .clickable(
-                interactionSource = blockInteractionSource,
-                indication = null,
-                onClick = {}
-            ),
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        awaitPointerEvent(pass = PointerEventPass.Initial).changes.forEach {
+                            it.consume()
+                        }
+                    }
+                }
+            },
         contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator(
